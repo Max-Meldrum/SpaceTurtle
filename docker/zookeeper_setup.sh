e761253a66d0cf7ee72b97524adabd24b11e5fa0
Max Meldrum <max@meldrum.se>
2017-04-16 00:20:02 +0200

    Add sleep time for now

--- docker/zookeeper_setup.sh
+++ docker/zookeeper_setup.sh
@@ -2,6 +2,10 @@
 
 # Set up container
 docker run -d --name zookeeper --net=host meldrum/docker-zookeeper-kerberos
+
+# TODO: Fix this
+echo "Waiting while kerberos load data..."
+sleep 20
 # Copy our Client JaaS file over to conf directory
 docker cp zookeeper:/opt/zookeeper-3.5.2-alpha/conf/zookeeper-client.keytab ../conf/zookeeper-client.keytab
 
