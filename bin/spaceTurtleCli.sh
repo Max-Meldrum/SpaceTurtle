e7fc0aa4bf253ae6afd14368bf17b7ca6c435724
Max Meldrum <max@meldrum.se>
2017-04-17 01:03:05 +0200

    Add security flags to cli script

--- bin/spaceTurtleCli.sh
+++ bin/spaceTurtleCli.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-java -jar -Dconfig.file=conf/spaceturtle.conf build/SpaceTurtleCli.jar
+java -jar -Dconfig.file=conf/spaceturtle.conf -Djava.security.auth.login.config=conf/jaas.conf -Djava.security.krb5.conf=conf/krb5.conf build/SpaceTurtleCli.jar
